47e91866a9eeb2110399c23e476a2a14100e3230
Anton <odianboy@mail.ru>
2022-05-31 23:46:25 +0300

    add style for page reg

--- src/pages/registration/registration.page.ts
+++ src/pages/registration/registration.page.ts
@@ -9,6 +9,7 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class RegistrationPage {
   form: FormGroup;
+  hide = true;
   nameIconWrite = 'mode_edit';
 
   constructor(private fb: FormBuilder) {
@@ -19,6 +20,26 @@ export class RegistrationPage {
     return this.fb.group({
       name: new FormControl(null, [Validators.required]),
       mobile: new FormControl(null, [Validators.required, Validators.minLength(10)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required, Validators.minLength(10)]),
     })
+  }
+
+  get prefixName(): string {
+    return this.form.controls['mobile'].dirty ? '+7' : '';
+  }
+
+  get iconVisibility(): string {
+    return this.hide ? 'visibility_off' : 'visibility';
+  }
+
+  get typeInput(): string {
+    return this.hide ? 'password' : 'text';
+  }
+
+  submit() {
+    console.log(this.form.getRawValue());
+    
+    this.form.reset();
   }
 }
